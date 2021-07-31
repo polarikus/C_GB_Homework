b812ec5f65714634af98c13e74020efb0f12cae2
Igor Danilov <polarikus@yandex.ru>
2021-07-31 13:19:58 +0300

    Homework 7

--- main.c
+++ main.c
@@ -3,41 +3,17 @@
 #include <stdlib.h>
 
 
-//Lesson 7
+//Lesson 8
 
-void multiAriphmetics(int64_t *summ, int32_t *diff, int64_t *mult, int32_t *div);
+char chars[] get_input(void);
 
 int main(int argc, char **argv) {
-    int64_t summ; //Sum of numbers
-    int32_t diff; //Difference of numbers
-    int64_t mult; //Multiplication of numbers
-    int32_t div; //Division of numbers
-    printf("Enter 2 numbers in turn, after entering each of the numbers press 'Enter':\n");
-    multiAriphmetics(&summ, &diff, &mult, &div);
-    printf("summ %"PRId64"\ndiff %"PRId32"\nmult %"PRId64"\ndiv %"PRId32"\n", summ, diff, mult, div);
-    return 0;
+
+printf('%c', get_input());
+return 0;
 }
 
+char chars[] get_input(void){
 
-void multiAriphmetics(int64_t *summ, int32_t *diff, int64_t *mult, int32_t *div){
-    int64_t input1; //First entered number
-    int64_t input2; //Second entered number
-    scanf("%"SCNd64"\n", &input1);
-    scanf("%"SCNd64, &input2);
-    if (input2 == 0){
-        printf("Error: Division by 0");
-        exit(1);
-    }
-    if (input1 < -2147483648 || input2 < -2147483648){
-        printf("Error: The entered number cannot be less than -2147483648");
-        exit(2);
-    }
-    if (input1 > 2147483647 || input2 > 2147483647){
-        printf("Error: The entered number cannot be more 2147483647");
-        exit(3);
-    }
-    *summ = input1 + input2;
-    *diff = input1 - input2;
-    *mult = input1 * input2;
-    *div = input1 / input2;
+
 }
